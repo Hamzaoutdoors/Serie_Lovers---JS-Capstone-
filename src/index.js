0a67835ded36b4e7110cfa7c42fd7f225f80d5b1
Hamza <hamzaoutdoors@gmail.com>
2021-09-07 01:02:14 +0100

    Add fonts to the project

---
+++ src/index.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/no-unresolved */
+/* import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands'; */
+import './style.css';
