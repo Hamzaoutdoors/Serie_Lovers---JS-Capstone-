699bb60024f8eb8355a00b07d723d160a49c92c6
Hamza <hamzaoutdoors@gmail.com>
2021-09-07 19:01:10 +0100

    add loading icons to wait for data from the API

--- src/function/request.js
+++ src/function/request.js
@@ -3,6 +3,9 @@ const requestURL = 'https://api.tvmaze.com/shows';
 const getShows = async () => {
   const response = await fetch(requestURL);
   const list = await response.json();
+  if (list) {
+    document.getElementById('loading').style.display = 'none';
+  }
   return list;
 };
 
