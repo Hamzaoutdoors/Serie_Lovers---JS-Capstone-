bbca29e5f9e49c2302b0e83032c9b30bbcb1200c
Hamza <hamzaoutdoors@gmail.com>
2021-09-07 17:01:01 +0100

    style items in the home page

--- src/dom/homePage.js
+++ src/dom/homePage.js
@@ -3,13 +3,16 @@ import getShows from '../function/request.js';
 const homePage = async () => {
   const container = document.getElementById('homePage');
   let myShows = await getShows();
-  myShows = myShows.slice(1, 9);
+  myShows = myShows.slice(0, 9);
   myShows.forEach((show) => {
     const showDiv = document.createElement('div');
+    showDiv.classList.add('col-4');
     const myImage = document.createElement('img');
+    myImage.classList.add('w-100', 'h-75');
     const imageUrl = show.image.original;
     myImage.setAttribute('src', imageUrl);
     const movieTitle = document.createElement('h5');
+    movieTitle.classList.add('p-2');
     movieTitle.innerHTML = show.name;
 
     showDiv.appendChild(myImage);
